31e6eaead1b97e86ad96d5f197c64499950f4e26
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-01 20:45:17 -0700

    Created register component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar";
-import Settings from "./pages/settings/Settings";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+// import Settings from "./pages/settings/Settings";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
 // import Write from "./pages/write/Write";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className='App'>
       <>
         <Topbar />
-        <Settings />
+        <Register />
       </>
     </div>
   );
