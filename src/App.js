58f2b90aeae5affc9dba36b4f1f5b344cc49f36b
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-01 21:11:59 -0700

    Created routes to components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React from "react";
 import Topbar from "./components/topbar/Topbar";
-// import Login from "./pages/login/Login";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-// import Settings from "./pages/settings/Settings";
-// import Home from "./pages/home/Home";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write";
-import React from "react";
+import Settings from "./pages/settings/Settings";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 
+
 function App() {
+  const user = false;
   return (
-    <div className='App'>
-      <>
+    <Router>
         <Topbar />
-        <Register />
-      </>
-    </div>
+        <Switch>
+          <Route exact path ='/'>
+            <Home />
+          </Route>
+          <Route path ='/register'>
+           {user ? <Home/> : <Register />}
+          </Route>
+          <Route path ='/login'>
+            {user ? <Home /> : <Login />}
+          </Route>
+          <Route path ='/write'>
+           { user ? <Write/> :  <Register />}
+          </Route>
+          <Route path ='/settings'>
+            {user ? <Settings /> : <Register />}
+          </Route>
+          <Route path ='/post/:postId'>
+            <Single />
+          </Route>
+          <Route path ='/settings'>
+            <Settings />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,6 +1,8 @@
 import "./topbar.css";
+import { Link } from 'react-router-dom'
 
 export default function Topbar() {
+  const user = false;
   return (
     <div className='top'>
       <div class='topLeft'>
@@ -11,18 +13,33 @@ export default function Topbar() {
       </div>
       <div class='topCenter'>
         <ul class='topList'>
-          <li className='topListItem'>HOME</li>
-          <li className='topListItem'>ABOUT</li>
-          <li className='topListItem'>CONTACT</li>
-          <li className='topListItem'>LOGOUT</li>
+          <li className='topListItem'>
+            <Link className='link' exact to ='/'>HOME</Link>
+          </li>
+          <li className='topListItem'><Link className='link' to ='/about'>ABOUT</Link></li>
+          <li className='topListItem'><Link className='link' to ='/contact'>CONTACT</Link></li>
+          <li className='topListItem'><Link className='link' to ='/write'>WRITE</Link></li>
+          <li className='topListItem'>
+            { user && 'LOGOUT'}
+          </li>
         </ul>
       </div>
       <div class='topRight'>
-        <img
-          className='topImg'
-          src='https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-          alt=''
-        />
+        {
+          user ? (
+            <img
+            className='topImg'
+            src='https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+            alt=''
+          />
+          ) : (
+            <ul className='topList'>
+            <li className='topListItem'><Link className='link' to ='/login'>LOGIN</Link></li>
+            <li className='topListItem'><Link className='link' to ='/register'>REGISTER</Link></li>
+            </ul>
+          )
+        }
+       
         <i className='topSearchIcon fas fa-search'></i>
       </div>
     </div>
