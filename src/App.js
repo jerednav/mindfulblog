ca8d930b41c7ff5ee1d46b5ff7227f5e58d7911d
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-01 17:02:20 -0700

    Created settings page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar";
+import Settings from "./pages/settings/Settings";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div className='App'>
       <>
         <Topbar />
-        <Write />
+        <Settings />
       </>
     </div>
   );
---
+++ src/pages/settings/Settings.js
@@ -0,0 +1,38 @@
+import './settings.css'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+export default function Settings() {
+    return (
+        <div className='settings'>
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update Your Account</span>
+                    <span className="settingsDeleteeTitle">Delete Your Account</span>
+                </div>
+                <form className="settingsForm">
+                    <label>Profile Picture</label>
+                    <div className="settingsPP">
+                        <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                        <label htmlFor='fileInput'>
+                        <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+                        </label>
+                        <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsPPInput"
+            />
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder='Jered'/>
+                    <label>Email</label>
+                    <input type="email" placeholder='jerednav@gmail.com'/>
+                    <label>Password</label>
+                    <input type="password"/>
+                    <button className="settingsSubmit">Update</button>
+                </form>
+            </div>
+            <Sidebar />
+        </div>
+    )
+}
